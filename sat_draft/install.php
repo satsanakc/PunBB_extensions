6889b0d1f23372886daad6d998a07e2080bf6d46
satsanakc <satsana@mail.ru>
2025-02-04 09:31:38 +0300

    Создание расширения sat_draft
    Внесение изменений в БД.

---
+++ sat_draft/install.php
@@ -0,0 +1,33 @@
+<?php
+if(!defined('SAT_DRAFT_INSTALL')) die();
+
+if (!$forum_db->field_exists('users', 'drafts'))
+    $forum_db->add_field('users', 'drafts', 'INT(10)', false, 5);
+
+if (!$forum_db->table_exists('sat_drafts')) {
+    $schema = array(
+        'FIELDS'      => array(
+            'save_name'          => array(
+                'datatype'        => 'VARCHAR(100)',
+                'allow_null'      => true
+            ),
+            'user_id'         	=> array(
+                'datatype'        => 'INT(10) UNSIGNED',
+                'allow_null'      => false
+            ),
+            'topic_id'         	=> array(
+                'datatype'        => 'INT(10) UNSIGNED',
+                'allow_null'      => true
+            ),
+            'message'       => array(
+                'datatype'        => 'TEXT',
+                'allow_null'      => true
+            )
+        ),
+        'PRIMARY KEY' => array('id'),
+	'INDEXES'	=> array(
+		'uid_idx'		=> array('user_id')
+	)
+    );
+    $forum_db->create_table('sat_drafts', $schema);
+}
