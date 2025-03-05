03abe50d7a95f22d4dee055f71bc47356565b7e0
satsanakc <satsana@mail.ru>
2025-03-05 20:12:49 +0300

    js-скрипты

---
+++ sat_dice/script.js
@@ -0,0 +1,23 @@
+function diceRoll() {
+  let str = '[roll=';
+  str += +$('#bbcode_dice [name="num"]').val() + ',';
+  str += +$('#bbcode_dice [name="faces"]').val() + ',';
+  str += +$('#bbcode_dice [name="afaces"]').val() + ',';
+  str += +$('#bbcode_dice [name="mod"]').val() + ',';
+  str += +$('#bbcode_dice [name="diff"]').val() + ',';
+  str += +$('#bbcode_dice [name="min"]')[0].checked + ',';
+  str += +$('#bbcode_dice [name="max"]')[0].checked + ',';
+  str += +$('#bbcode_dice [name="sum"]')[0].checked + ',';
+  str += $('#bbcode_dice [name="desc"]').val() + ']';
+  PUNBB.pun_bbcode.insert_text('', str);
+  $('#bbcode_dice').hide();
+}
+
+$('#sat_dice_afaces_row').hide();
+$('#bbcode_dice .extra').hide();
+$('#bbcode_dice select[name="faces"]').on('change', function() {
+  if(this.value)
+    $('#sat_dice_afaces_row').hide();
+  else
+    $('#sat_dice_afaces_row').show();
+});
